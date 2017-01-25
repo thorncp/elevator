523c6793cb7cfb6a17ca21fc10c54146e1a4ea46
Chris Thorn <chris@thorn.co>
2017-01-24 19:20:11 -0800

    Add elevator object
    * Door opens and closes when spacebar is pressed.
    * Adds simulator VR rig controlled with mouse + WASD

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject InnerDoor;
+    public GameObject OuterDoor;
+    public float DoorSpeed;
+
+    private bool isOpening;
+    private Vector3 startingInnerDoorPosition;
+    private float innerDoorWidth;
+    private const float Buffer = 0.001f;
+
+    private void Start()
+    {
+        startingInnerDoorPosition = InnerDoor.transform.localPosition;
+        innerDoorWidth = InnerDoor.GetComponent<Renderer>().bounds.size.z;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isOpening = !isOpening;
+        }
+
+        if (isOpening && !IsOpen())
+        {
+            InnerDoor.transform.localPosition = new Vector3(
+                InnerDoor.transform.localPosition.x + DoorSpeed,
+                InnerDoor.transform.localPosition.y,
+                InnerDoor.transform.localPosition.z
+            );
+            OuterDoor.transform.localPosition = new Vector3(
+                OuterDoor.transform.localPosition.x + DoorSpeed * 2,
+                OuterDoor.transform.localPosition.y,
+                OuterDoor.transform.localPosition.z
+            );
+        }
+        else if (!isOpening && !IsClosed())
+        {
+            InnerDoor.transform.localPosition = new Vector3(
+                InnerDoor.transform.localPosition.x - DoorSpeed,
+                InnerDoor.transform.localPosition.y,
+                InnerDoor.transform.localPosition.z
+            );
+            OuterDoor.transform.localPosition = new Vector3(
+                OuterDoor.transform.localPosition.x - DoorSpeed * 2,
+                OuterDoor.transform.localPosition.y,
+                OuterDoor.transform.localPosition.z
+            );
+        }
+    }
+
+    private bool IsClosed()
+    {
+        return DistanceFromStartingPosition() <= Buffer;
+    }
+
+    private bool IsOpen()
+    {
+        return DistanceFromStartingPosition() >= innerDoorWidth - Buffer;
+    }
+
+    private float DistanceFromStartingPosition()
+    {
+        return Math.Abs(InnerDoor.transform.localPosition.x - startingInnerDoorPosition.x);
+    }
+}
